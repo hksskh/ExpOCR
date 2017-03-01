77b1b9a3641fea44f910b648ab32b9007bf543ec
Brianna Ifft <briannaifft@Briannas-MacBook-Pro.local>
2017-02-28 21:09:47 -0600

    started building expense list for a friend

--- app/src/main/java/com/example/mihika/expocr/IndividualFriendActivity.java
+++ app/src/main/java/com/example/mihika/expocr/IndividualFriendActivity.java
@@ -2,12 +2,30 @@ package com.example.mihika.expocr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class IndividualFriendActivity extends AppCompatActivity {
 
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual_friend);
+
+        mListView = (ListView) findViewById(R.id.expenses_list_view);
+        final ArrayList<Expense> expenseList = Expense.getRecipesFromFile("recipes.json", this);
+
+        String[] listItems = new String[expenseList.size()];
+
+        for(int i = 0; i < expenseList.size(); i++){
+            Expense expense = expenseList.get(i);
+            listItems[i] = expense.expense;
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        mListView.setAdapter(adapter);
     }
 }
