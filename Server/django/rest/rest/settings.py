10d56a3034f600f18757f40e8b9708796d17d1d7
kesongh2 <kesongh2@illinois.edu>
2017-04-10 08:28:13 -0500

    resotre mysql password in settings.py

--- Server/django/rest/rest/settings.py
+++ Server/django/rest/rest/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ExpOCR',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'UIUCcs428SP17',
         'HOST': 'localhost',
         'PORT': '3306',
     }
