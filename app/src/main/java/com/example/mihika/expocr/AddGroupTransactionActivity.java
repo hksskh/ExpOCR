b649329d16560b01a4e064e633ec5f4a96290834
Brianna Ifft <bifft2@illinois.edu>
2017-04-14 18:19:24 -0500

    added group transaction

---
+++ app/src/main/java/com/example/mihika/expocr/AddGroupTransactionActivity.java
@@ -0,0 +1,261 @@
+package com.example.mihika.expocr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.example.mihika.expocr.util.ServerUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Vector;
+
+/**
+ * Created by briannaifft on 4/13/17.
+ */
+
+public class AddGroupTransactionActivity extends AppCompatActivity {
+    private MultiSelectionSpinner userSpinner;
+    private Spinner categorySpinner;
+    private AutoCompleteTextView amount_text;
+    private AutoCompleteTextView memo_text;
+    private Handler handler;
+    private ArrayList<Integer> groupMemberArray;
+    private final String TAG = "AddGroupTransactionActivity";
+    private HashMap<String, Integer> nameIdMap;
+
+    private int g_id;
+    private String g_name;
+
+    private static final String[] amount_autos = new String[]{
+            "1", "10", "100", "1000"
+    };
+    private static final String[] memo_autos = new String[]{
+            "Movie", "Snack", "Popcorn"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_group_transaction);
+
+        Intent inIntent = getIntent();
+        g_id = inIntent.getIntExtra("group_id", 1);
+        g_name = inIntent.getStringExtra("group_name");
+
+        userSpinner = (MultiSelectionSpinner) findViewById(R.id.group_transaction_user_spinner);
+        categorySpinner = (Spinner) findViewById(R.id.group_transaction_category_spinner);
+        addEntriesForSpinner();
+        set_autotext_adapters();
+
+        Button add_transaction_button = (Button) findViewById(R.id.add_group_transaction_button);
+        add_transaction_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptSendData();
+            }
+        });
+
+        handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch(msg.what){
+
+                }
+            }
+        };
+    }
+
+    protected void addEntriesForSpinner() {
+        List<String> list = new ArrayList<String>();
+
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+
+                String url = "http://" + ServerUtil.getServerAddress() + "group/get_members";
+                StringBuilder requestString = new StringBuilder();
+                requestString.append("g_id=").append(g_id);
+
+//                Log.d(TAG, requestString.toString());
+                String response = ServerUtil.sendData(url, requestString.toString(), "UTF-8");
+
+//                Log.d(TAG, "From server:" + response);
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    groupMemberArray = new ArrayList<Integer>();
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        int u_id = jsonObject.getInt("u_id");
+                        groupMemberArray.add(u_id);
+                    }
+
+                    userSpinner.setItems(getNamesFromIds());
+
+                } catch (JSONException jsex){
+                    jsex.printStackTrace();
+                }
+
+            }
+        }).start();
+
+        userSpinner.setItems(list);
+
+    }
+
+    private ArrayList<String> getNamesFromIds() {
+        final ArrayList<String> spinnerNames = new ArrayList<String>();
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+
+                for(int i =0; i< groupMemberArray.size(); i++) {
+                    if(!(groupMemberArray.get(i) == MainActivity.getU_id())) {
+                        String url = "http://" + ServerUtil.getServerAddress() + "user/get_user_by_id";
+                        StringBuilder requestString = new StringBuilder();
+                        requestString.append("id=").append(groupMemberArray.get(i));
+                        String response = ServerUtil.sendData(url, requestString.toString(), "UTF-8");
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String name = jsonObject.getJSONObject("fields").getString("U_Name");
+                            String email = jsonObject.getJSONObject("fields").getString("Email");
+                            nameIdMap.put(name + " (" + email + ")", groupMemberArray.get(i));
+                            spinnerNames.add(name + " (" + email + ")");
+
+                        } catch (JSONException jsex) {
+                            jsex.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+        return null;
+    }
+
+    private void set_autotext_adapters(){
+        //adapters for AutoCompleteTextViews
+        ArrayAdapter<String> amount_adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, amount_autos);
+        amount_text = (AutoCompleteTextView)
+                findViewById(R.id.add_transaction_amount);
+        amount_text.setAdapter(amount_adapter);
+        ArrayAdapter<String> memo_adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, memo_autos);
+        memo_text = (AutoCompleteTextView)
+                findViewById(R.id.add_transaction_memo);
+        memo_text.setAdapter(memo_adapter);
+    }
+
+    public void attemptSendData() {
+        boolean cancel = false;
+        View focusView = null;
+        String amount = amount_text.getText().toString();
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(amount)|| !isAmountValid(amount)) {
+            amount_text.setError(getString(R.string.error_invalid_amount));
+            focusView = amount_text;
+            cancel = true;
+            focusView.requestFocus();
+        }
+
+        if (!cancel) {
+            sendData();
+        }
+    }
+
+    public void sendData() {
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+                Date date = new Date();
+                String datetime = dateFormat.format(date);
+
+                List<String> selectedNames = userSpinner.getSelectedStrings();
+
+                double amount = Double.parseDouble(amount_text.getText().toString());
+                double individualAmount = ((amount/selectedNames.size())*(-100));
+                individualAmount = Math.round(individualAmount);
+                individualAmount /= 100;
+
+                for(int i = 0; i < selectedNames.size(); i++) {
+                    int userId = nameIdMap.get(selectedNames.get(i));
+
+                    String url = "http://" + ServerUtil.getServerAddress() + "group/add_transaction";
+                    StringBuilder requestString = new StringBuilder();
+                    requestString.append("receiver_id=").append(userId)
+                            .append("&group_id").append(g_id)
+                            .append("&category=").append(categorySpinner.getSelectedItem().toString())
+                            .append("&memo=").append(memo_text.getText())
+                            .append("&amount=")
+                            .append(individualAmount)
+                            .append("&date=").append(datetime);
+//                    Log.d(TAG, requestString.toString());
+                    String response = ServerUtil.sendData(url, requestString.toString(), "UTF-8");
+
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        if (jsonObject.has("warning")) {
+                           // handle it
+                        }
+                    } catch (JSONException jsex){
+                        jsex.printStackTrace();
+                    }
+                }
+
+                String url = "http://" + ServerUtil.getServerAddress() + "group/add_transaction";
+                StringBuilder requestString = new StringBuilder();
+                requestString.append("receiver_id=").append(MainActivity.getU_id())
+                        .append("&group_id").append(g_id)
+                        .append("&category=").append(categorySpinner.getSelectedItem().toString())
+                        .append("&memo=").append(memo_text.getText())
+                        .append("&amount=")
+                        .append(individualAmount*selectedNames.size()*(-1))
+                        .append("&date=").append(datetime);
+//                    Log.d(TAG, requestString.toString());
+                String response = ServerUtil.sendData(url, requestString.toString(), "UTF-8");
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    if (jsonObject.has("warning")) {
+                        // handle it
+                    } else {
+                        Intent gotoIndividualActivity = new Intent(AddGroupTransactionActivity.this, IndividualGroupActivity.class);
+                        gotoIndividualActivity.putExtra("group_id", g_id);
+                        gotoIndividualActivity.putExtra("group_name", g_name);
+                        startActivity(gotoIndividualActivity);
+                    }
+                } catch (JSONException jsex){
+                    jsex.printStackTrace();
+                }
+
+            }
+        }).start();
+    }
+
+    private boolean isAmountValid(String amount) {
+        return amount.matches("^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\\.[0-9]{2}");
+
+    }
+
+}
