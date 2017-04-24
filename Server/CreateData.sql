1ac3ed5114803addba24138d750287c78f11e444
kesongh2 <kesongh2@illinois.edu>
2017-04-23 20:02:14 -0500

    fix createdata.sql about primary keys

--- Server/CreateData.sql
+++ Server/CreateData.sql
@@ -12,33 +12,33 @@ INSERT INTO `ExpOCR`.`TRANSACTIONS` (`T_Id`, `Sender_Id`, `Receiver_Id`, `Catego
 
 #CREATE SOME GROUPS
 INSERT INTO GROUPS VALUES
-(0, 'All The Buddies'),
-(1, 'Only Half The Buddies'),
-(2, 'A Third of The Buddies'),
-(3, 'A Fourth of The Buddies');
+(1, 'All The Buddies'),
+(2, 'Only Half The Buddies'),
+(3, 'A Third of The Buddies'),
+(4, 'A Fourth of The Buddies');
 
 #CREATE SOME MEMBERS
 INSERT INTO MEMBERS VALUES
-(0,1),
-(0,2),
-(0,3),
-(0,4),
-(0,5),
-(1,1),
-(1,3),
-(1,5),
-(2,4);
+(1, 1,1),
+(2, 1,2),
+(3, 1,3),
+(4, 1,4),
+(5, 1,5),
+(6, 2,1),
+(7, 2,3),
+(8, 2,5),
+(9, 3,4);
 
 #CREATE SOME GROUP TRANSACTIONS
 INSERT INTO GROUP_TRANSACTIONS VALUES
-(0,0,1,'Food','Dinner with the gang', '40', '2017-05-01 11:32:09'),
-(1,0,2,'Food','Dinner with the gang', '-10', '2017-05-01 11:32:09'),
-(2,0,3,'Food','Dinner with the gang', '-10', '2017-05-01 11:32:09'),
-(3,0,4,'Food','Dinner with the gang', '-10', '2017-05-01 11:32:09'),
-(4,0,5,'Food','Dinner with the gang', '-10', '2017-05-01 11:32:09'),
-(5,0,3,'Food','Dinner with the gang AGAIN', '60', '2017-05-01 12:32:09'),
-(6,0,4,'Food','Dinner with the gang AGAIN', '-35', '2017-05-01 12:32:09'),
-(7,0,5,'Food','Dinner with the gang AGAIN', '-25', '2017-05-01 12:32:09');
-(8,0,4,'Food','Dinner with the gang AGAIN 2.0', '10', '2017-06-01 12:32:09'),
-(9,0,1,'Food','Dinner with the gang AGAIN 2.0', '-10', '2017-06-01 12:32:09');
+(1,1,1,'Food','Dinner with the gang', '40', '2017-05-01 11:32:09'),
+(2,1,2,'Food','Dinner with the gang', '-10', '2017-05-01 11:32:09'),
+(3,1,3,'Food','Dinner with the gang', '-10', '2017-05-01 11:32:09'),
+(4,1,4,'Food','Dinner with the gang', '-10', '2017-05-01 11:32:09'),
+(5,1,5,'Food','Dinner with the gang', '-10', '2017-05-01 11:32:09'),
+(6,1,3,'Food','Dinner with the gang AGAIN', '60', '2017-05-01 12:32:09'),
+(7,1,4,'Food','Dinner with the gang AGAIN', '-35', '2017-05-01 12:32:09'),
+(8,1,5,'Food','Dinner with the gang AGAIN', '-25', '2017-05-01 12:32:09');
+(9,1,4,'Food','Dinner with the gang AGAIN 2.0', '10', '2017-06-01 12:32:09'),
+(10,1,1,'Food','Dinner with the gang AGAIN 2.0', '-10', '2017-06-01 12:32:09');
 
