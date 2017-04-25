59b529f9fb946274e47608ac6011492e191beb61
Brianna Ifft <bifft2@illinois.edu>
2017-04-25 15:21:17 -0500

    added tests for transactions in backend

--- Server/django/rest/expocr_transaction/tests.py
+++ Server/django/rest/expocr_transaction/tests.py
@@ -17,11 +17,8 @@ class TransactionTestCase(TestCase):
         Transaction.create_transaction(sender_id="2", receiver_id="3", category="Apartment", memo="Toilet Paper",
                                        amount="10.00", date=self.current_time)
         Transaction.create_transaction(sender_id="2", receiver_id="2", category="Apartment", memo="Rent",
-                                       amount="-500.00", date=self.current_time)
+                                       amount="500.00", date=self.current_time)
 
-    def test_get_all_transactions(self):
-        test = Transaction.get_all_transactions()
-        self.assertEqual(test.count(), 4)
 
     def test_transaction_is_created(self):
         test = Transaction.get_transaction_by_sender_id(sender_id="1")
@@ -30,37 +27,39 @@ class TransactionTestCase(TestCase):
         self.assertEqual(test[0]['Amount'], 10.00)
         self.assertTrue(str(test[0]['Date']).startswith(self.current_time))
 
-    def test_transaction_get_receivers(self):
-        test = Transaction.get_all_receivers(sender_id="2")
-        self.assertEqual(int(test[0]['Receiver_Id']), 2)
-        self.assertEqual(int(test[1]['Receiver_Id']), 3)
-        self.assertEqual(len(test), 2)
-        self.assertEqual(test[1]['Amount__sum'], 20.00)
-
     def test_transaction_get_transaction_between(self):
         test = Transaction.get_transaction_between(sender_id=2, receiver_id=3)
         self.assertEqual(len(test), 2)
         for transaction in test:
             self.assertEqual(transaction['Amount'], 10.00)
 
-    def test_get_entertain_transaction(self):
-        test = Transaction.get_entertain_transaction(sender_id=1)
-        self.assertEqual(test.count(), 1)
-        self.assertEqual(test[0].Memo, 'Lego Movie')
-
-    def test_count_sender_transaction(self):
-        test = Transaction.count_sender_transaction(sender_id=2)
-        self.assertEqual(test, 3)
-
-    def test_update_Memo(self):
-        test = Transaction.get_entertain_transaction(sender_id=1)
-        t_id = test[0].T_Id
-        Transaction.update_Memo(t_id, 'Lego Batman Movie')
-        test = Transaction.get_entertain_transaction(sender_id=1)
-        self.assertEqual(test[0].Memo, 'Lego Batman Movie')
 
     def test_delete_transaction_between(self):
         test = Transaction.delete_transaction_between(sender_id=2, receiver_id=3)
         self.assertEqual(test[0], 2)
         test = Transaction.get_transaction_between(sender_id=2, receiver_id=3)
         self.assertEqual(test.count(), 0)
+
+    def test_get_transaction_by_receiver_id(self):
+        test = Transaction.get_transaction_by_receiver_id(3)
+        self.assertTrue(len(test) == 2)
+        self.assertEqual(test[0]['Receiver_Id'], 3)
+        self.assertEqual(test[1]['Receiver_Id'], 3)
+
+    def test_get_transaction_by_id(self):
+        test = Transaction.get_transaction_by_id(2)
+        self.assertTrue(len(test) == 4)
+
+    def test_get_all_friends_sender(self):
+        test = Transaction.get_all_friends_sender(2)
+        self.assertTrue(len(test), 2)
+        self.assertTrue(test[0]['Receiver_Id'] == 2 or test[0]['Receiver_Id'] == 3)
+        self.assertTrue(test[1]['Receiver_Id'] == 2 or test[1]['Receiver_Id'] == 3)
+        self.assertTrue(test[0]['Receiver_Id'] != test[1]['Receiver_Id'])
+
+    def test_get_all_friends_receiver(self):
+        test = Transaction.get_all_friends_receiver(2)
+        self.assertTrue(len(test), 2)
+        self.assertTrue(test[0]['Sender_Id'] == 1 or test[0]['Sender_Id'] == 2)
+        self.assertTrue(test[1]['Sender_Id'] == 1 or test[1]['Sender_Id'] == 2)
+        self.assertTrue(test[0]['Sender_Id'] != test[1]['Sender_Id'])
