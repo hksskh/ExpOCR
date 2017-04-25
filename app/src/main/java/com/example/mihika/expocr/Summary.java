d3bdbdc967a5c1a3c0d5585696e502ef5ad8dcf4
Hermite Bai <hermitebai@outlook.com>
2017-04-25 02:17:15 -0500

    Bug Fixed: truncate decimal, color, remove payment from category

--- app/src/main/java/com/example/mihika/expocr/Summary.java
+++ app/src/main/java/com/example/mihika/expocr/Summary.java
@@ -264,7 +264,7 @@ public class Summary extends AppCompatActivity {
         }
         Map<String, Double> percents = new HashMap<>();
         for (String key : amounts.keySet()) {
-            percents.put(key, 100 * (BigDecimal.valueOf(amounts.get(key) / totalAmount).setScale(4, RoundingMode.HALF_UP).doubleValue()));
+            percents.put(key, (BigDecimal.valueOf(amounts.get(key) * 100 / totalAmount).setScale(2, RoundingMode.HALF_UP).doubleValue()));
         }
         if (percents.size() == 0) {
             Map<String, Double> ret = new HashMap<>();
